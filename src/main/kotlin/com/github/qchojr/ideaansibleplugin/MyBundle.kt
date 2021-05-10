109bafbd29cb22ba38512e61f9bbc08a32411866
GitHub Action <action@github.com>
2021-05-10 06:31:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/qchojr/ideaansibleplugin/MyBundle.kt
+++ src/main/kotlin/com/github/qchojr/ideaansibleplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.qchojr.ideaansibleplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/qchojr/ideaansibleplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/qchojr/ideaansibleplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.qchojr.ideaansibleplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qchojr.ideaansibleplugin.MyBundle
 
 class MyApplicationService {
 
